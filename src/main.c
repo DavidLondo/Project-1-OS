000fda3cfea3f898d02c77dcf3bdebea125bfe7d
DavidLondo <davidlondonopalacio@gmail.com>
2025-07-25 16:20:40 -0500

    fix(main): Enable opening a file from command line argument. Closes #1

--- src/main.c
+++ src/main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include "Utils/Headers/aux.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    FILE *fptr;
-
-    fptr = fopen("test.txt", "r");
-
-    char process_list[100];
-
-    if (fptr != NULL) {
-        // Logic
-        // while(fgets(fptr, 100, fptr)) {
-        //     printf("%s", fptr);
-        // }
-    } else {
-        printf("Error opening file.\n");
+    if (argc < 2) {
+        printf("Usage: %s <archivo>\n", argv[0]);
         return 1;
     }
 
-    fclose(fptr);
+    FILE *file = fopen(argv[1], "r");
+    
+        if (file != NULL) {
+            // Logic
+            // while(fgets(fptr, 100, fptr)) {
+            //     printf("%s", fptr);
+            // }
+        } else {
+            printf("Error opening file.\n");
+            return 1;
+        }
+
+    char process_list[100]; // Define size for the buffer
+
+    fclose(file);
+
+    return 0;
 }
