98a615cc0ad8afcc73222beffb33b77b98a2e0ff
Juan Jara <jaracalle2041@gmail.com>
2025-08-01 17:39:40 -0500

    feat(process): Implement the structs and logic for process and instructions

--- src/Models/process.c
+++ src/Models/process.c
@@ -1,1 +1,51 @@
-#include "../Utils/Headers/aux.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../Utils/Headers/process.h"
+
+
+Process* process_create(int pid, int ax, int bx, int cx, int quantum, Instruction* instructions, int num_inst) {
+    Process* p = (Process*)malloc(sizeof(Process));
+    // Variable asignation
+    p->pid = pid;
+    p->pc = 0;
+    p->ax = ax;
+    p->bx = bx;
+    p->cx = cx;
+    p->quantum = quantum;
+    strcpy(p->state, "Ready");
+    p->num_instructions = num_inst;
+    p->instructions = (Instruction*)malloc(sizeof(Instruction) * num_inst);
+
+    for (int i = 0; i < num_inst; ++i) {
+        p->instructions[i] = instructions[i]; // copy instructions
+    }
+
+    return p;
+}
+
+void process_free(Process* p) {
+    if (p) {
+        free(p->instructions);
+        free(p);
+    }
+}
+
+void process_execute_instruction(Process* p) {
+    if (p->pc >= p->num_instructions) return;
+
+    // Process the instruction in the position of program counter
+    Instruction* instr = &p->instructions[p->pc];
+    printf("    Executing instruction [%d]: ", p->pc);
+    execute_instruction(instr, p);
+    p->pc++;
+}
+
+int process_has_finished(const Process* p) {
+    return p->pc >= p->num_instructions;
+}
+
+void process_print_state(const Process* p) {
+    printf("  PID=%d | PC=%d | AX=%d | BX=%d | CX=%d | State=%s\n",
+        p->pid, p->pc, p->ax, p->bx, p->cx, p->state);
+}
--- src/Utils/Headers/process.h
+++ src/Utils/Headers/process.h
@@ -1,9 +1,22 @@
-#include "aux.h"
+#ifndef PROCESS_H
+#define PROCESS_H
 
-typedef struct {
+#include "instruction.h"
+
+typedef struct Process {
     int pid;
     int pc;
     int ax, bx, cx;
     int quantum;
-    char estado[10];
+    char state[10];
+    Instruction* instructions;
+    int num_instructions;
 } Process;
+
+Process* process_create(int pid, int ax, int bx, int cx, int quantum, Instruction* instructions, int num_inst); // Process constructor
+void process_free(Process* p); // free process
+void process_execute_instruction(Process* p); // Execute instruction
+int process_has_finished(const Process* p); 
+void process_print_state(const Process* p); // Actual state
+
+#endif
